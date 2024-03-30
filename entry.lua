5dbc5cbe243bfa70f2c1c8104d547f40fe25fa69
Federico Balducci <ott8v@proton.me>
2024-03-30 22:33:26 +0100

    first commit

---
+++ entry.lua
@@ -0,0 +1,34 @@
+local self_ID = "MB-326"
+
+declare_plugin(self_ID,
+{
+displayName = _("MB-326"),
+
+installed = true,
+dirName = current_mod_path,
+fileMenuName = _("MB-326"),
+info = _("MB-326 mod"),
+
+version = "1.0.0",
+state = "installed",
+encyclopedia_path = current_mod_path .. '/Encyclopedia',
+InputProfiles = {
+    ["MB-326"] = current_mod_path .. '/Input',
+},
+Skins = {
+    {
+        name = _("MB-326"),
+        dir = "Skins/1"
+    },
+},
+Options = {
+    {
+        name = _("MB-326"),
+        nameId = "MB-326",
+        dir = "Options",
+        CLSID = "{MB-326 options}"
+    }
+}
+})
+
+plugin_done()
